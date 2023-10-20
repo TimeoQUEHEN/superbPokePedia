dc0879c4f9018e823b60ab72b8dd023afcc2459d
Timeo Quehen <timeo_quehen@ens.univ-artois.fr>
2023-10-20 17:28:02 +0200

    modif code html/css

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',
+            static_folder='static',
+            template_folder='templates')
 
 @app.route("/")
 def affichage():
