971a606929c88f28429992f219038d09adc0a5b9
Timeo Quehen <timeo_quehen@ens.univ-artois.fr>
2023-10-20 16:56:42 +0200

    mise en place page d'affichage de pokemon spécifique

--- pok/main.py
+++ pok/main.py
@@ -20,3 +20,7 @@ app.secret_key = "Welcome to SPP"
 @app.route("/")
 def index():
     return render_template('index.html',var="sexe")
+
+@app.route("/poke")
+def pokemon():
+    return render_template('pokemon.html',pokemon_name="sexe",pok_name_fr="bite",pok_name_eng="dick",sprite="sprite",sprite_shiny="SS",sprite_region_diff="diff",stats_du_pok=["150","300"])
