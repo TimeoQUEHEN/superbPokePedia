785efaf73258c5679094131fbddcca663b6044c5
LapotreNicolas <nicolas62lapotre@gmail.com>
2024-01-10 22:45:01 +0100

    Test, ajout et correction de routes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 # python -m flask --app .\nom_du_fichier\ run
 
@@ -8,18 +8,35 @@ app = Flask(__name__, static_url_path='',
 
 
 @app.route("/")
-def affichage():
-    return render_template("index.html")
-
-
 @app.route("/<pokemon>", methods=["GET"])
-def pokemon(poke=None):
-    return render_template("pokemon.html", pokemon=poke)
+def pokemon(pokemon=None):
+    if pokemon is None:
+        return render_template("index.html")
+    return render_template("pokemon.html", pokemon=pokemon)
 
 
-@app.route("/account", methods=["POST"])
-def exo1():
+@app.route("/account", methods=["GET"])
+def account():
+    # Récupérer l'utilisateur grace au cookie
     return render_template("account.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        # Récupérer l'utilisateur dans la base de donnée (peut être vérifier s'il n'y pas d'erreur)
+        return redirect("/account")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        # Créer l'utilisateur dans la base de donnée (peut être vérifier s'il n'y pas d'erreur)
+        return redirect("/account")
 
 
 if __name__ == "__main__":
